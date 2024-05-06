3d4b26328119af5a1bae3d809637807b8495c8f1
eugeneLuck <eugeneLuck@gmail.com>
2024-05-06 16:47:49 +0300

    Add project files.

---
+++ Task5Info/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> numbers = new List<int>();
+for(int i=1;i<=50;i++)
+{
+    Console.WriteLine($"I:{i} R={SeacrhR(i)}");
+    if(SeacrhR(i) > 151)
+    {
+        numbers.Add(SeacrhR(i));
+    }
+}
+Console.WriteLine(numbers.Min());
+Console.ReadKey();
+int SeacrhR(int number)
+{
+    int R = 0;
+    int ost = number % 3;
+    switch(ost)
+    {
+        case 0:
+            R = number * 8 + number%8;
+            break;
+        case 1:
+            R = number * 4 + 3;
+            break;
+        case 2:
+            R = number * 8 + 6;
+            break;
+    }
+    return R;
+}
